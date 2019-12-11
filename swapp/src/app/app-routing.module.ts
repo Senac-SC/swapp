95bd74b01d54a4a40c9856431e39c4091111a6f4
Yakami <58188693+yakamijack@users.noreply.github.com>
2019-12-11 17:11:36 -0300

    criação da listagem
    criação da listagem beta 1 - 11/12/2019

--- swapp/src/app/app-routing.module.ts
+++ swapp/src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'listagem',
+    loadChildren: () => import('./listagem/listagem.module').then( m => m.ListagemPageModule)
   }
+
 ];
 
 @NgModule({
